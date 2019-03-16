f98cc9da41e1249cf0f64ef68ff1b16cca6c89a4
hiroki-okazaki <1031okazaki@gmail.com>
2019-03-16 15:12:07 +0900

    削除機能作成

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -33,7 +33,14 @@ class BooksController < ApplicationController
       render :edit
     end
   end
+  
 
+  def destroy
+    @book = Book.find(params[:id])
+    @book.destroy
+    redirect_to books_path, notice: "書籍を削除しました"
+  end
+  
   private
 
   def book_params
